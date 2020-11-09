c9f36520b00a8eb4047e2927d81dd3a584c64a9d
Riccardo Rizzo <37290960+Rocketct@users.noreply.github.com>
2020-11-09 09:23:01 +0100

    fixed Digital_out example and added CAN Examples
    Fixed digital out example and added CAN read and write Examples

---
+++ examples/CAN/ReadCan/ReadCan.ino
@@ -0,0 +1,56 @@
+/*
+  CAN Read Example
+
+  This sketch shows how to use the CAN transceiver on the Machine
+  Control and how to receive data from the RX CAN channel.
+
+  Circuit:
+   - Portenta H7
+   - Automation Carrier
+
+
+  created 25 August 2020
+  by Silvio Navaretti
+  modified 4 November 2020
+  by Riccardo Rizzo
+*/
+#include <AutomationCarrier.h>
+#include <CAN.h>
+
+using namespace automation;
+
+#define DATARATE_2MB     260000
+#define DATARATE_1_5MB   206000
+#define DATARATE_1MB     125000
+#define DATARATE_800KB   100000
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+
+  Serial.println("Start CAN initialization");
+  comm_protocols.enableCAN();
+  comm_protocols.can.frequency(DATARATE_800KB);
+  Serial.println("Initialization done");
+}
+
+
+void loop() {
+  mbed::CANMessage msg;
+  if (comm_protocols.can.read(msg)) {
+
+    // Print the sender ID
+    Serial.print("ID: ");
+    Serial.println(msg.id);
+
+    // Print the first Payload Byte
+    Serial.print("Message received:");
+    Serial.println(msg.data[0], DEC);
+
+  }
+
+  delay(100);
+}
--- examples/Temp_probes_RTD/Temp_probes_RTD.ino
+++ examples/Temp_probes_RTD/Temp_probes_RTD.ino
@@ -17,7 +17,6 @@ void setup() {
   temp_probes.setTH();
   temp_probes.selectChannel(0);
   temp_probes.rtd.begin(MAX31865_2WIRE);  // set to 2WIRE or 4WIRE as necessary
-  temp_probes.t.begin();
 }
 
 
@@ -57,14 +56,6 @@ void loop() {
   }
   Serial.println();
   delay(1000);
-
-  Serial.print("Reference temperature ");
-  Serial.print(temp_probes.t.readReferenceTemperature());
-  Serial.println(" °C");
-
-  Serial.print("Temperature ");
-  Serial.print(temp_probes.t.readTemperature());
-  Serial.println(" °C");
 
   Serial.println();
 }
