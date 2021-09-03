7297cb77e5cb332d40d953243608353662402f6a
Riccardo Rizzo <r.rizzo@arduino.cc>
2021-09-03 12:45:44 +0200

    added RS232 example

---
+++ examples/RS232/RS232.ino
@@ -0,0 +1,78 @@
+/*
+  RS232 communication
+
+  This sketch shows how to use the SP335ECR1 on the Machine
+  Control as a RS232 interface, how to periodically send
+  a string on the RS232 TX channel and how to receive data
+  from the interface RX channel.
+
+  Circuit:
+   - Portenta H7
+   - Machine Control
+   - A Slave device with RS232 interface
+   - Connect TXN to RXD Slave RS232 channel
+   - Connect RSXN to TXD Slave RS232 channel
+
+*/
+
+#include "Arduino_MachineControl.h"
+
+using namespace machinecontrol;
+
+constexpr unsigned long sendInterval { 1000 };
+unsigned long sendNow { 0 };
+
+unsigned long counter { 0 };
+
+void setup()
+{
+
+    Serial.begin(115200);
+    // Wait for Serial or start after 2.5s
+    for (auto const timeout = millis() + 2500; !Serial && timeout < millis(); delay(500))
+        ;
+
+    delay(2500);
+    Serial.println("Start RS232 initialization");
+
+    // Set the PMC Communication Protocols to default config
+    comm_protocols.init();
+
+    // RS485/RS232 default config is:
+    // - RS485 mode
+    // - Half Duplex
+    // - No A/B and Y/Z 120 Ohm termination enabled
+
+    // Enable the RS485/RS232 system
+    comm_protocols.rs485Enable(false);
+
+    // Specify baudrate, and preamble and postamble times for RS485 communication
+    comm_protocols.rs485.begin(115200, 0, 500);
+    // Start in receive mode
+    comm_protocols.rs485.receive();
+
+    Serial.println("Initialization done!");
+}
+
+void loop()
+{
+    if (comm_protocols.rs485.available())
+        Serial.write(comm_protocols.rs485.read());
+
+    if (millis() > sendNow) {
+        // Disable receive mode before transmission
+        comm_protocols.rs485.noReceive();
+
+        comm_protocols.rs485.beginTransmission();
+
+        comm_protocols.rs485.print("hello ");
+        comm_protocols.rs485.println(counter++);
+
+        comm_protocols.rs485.endTransmission();
+
+        // Re-enable receive mode after transmission
+        comm_protocols.rs485.receive();
+
+        sendNow = millis() + sendInterval;
+    }
+}
