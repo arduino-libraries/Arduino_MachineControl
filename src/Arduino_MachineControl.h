80beb874b3aa6ca8b88dc771949dc4d4e6fd0e7b
Martino Facchin <m.facchin@arduino.cc>
2022-09-29 11:59:17 +0200

    Invert TEMP channels on PMCr2

--- src/Arduino_MachineControl.h
+++ src/Arduino_MachineControl.h
@@ -13,6 +13,13 @@
 #include <pinDefinitions.h>
 #include <mbed.h>
 
+#if __has_include("portenta_info.h")
+#include "portenta_info.h"
+#define TRY_REV2_RECOGNITION
+uint8_t* boardInfo();
+#define PMC_R2_SKU  (24 << 8 | 3)
+#endif
+
 namespace machinecontrol {
 
 /**
@@ -28,6 +35,26 @@ public:
 	*  @param channel (0-2)
 	*/   
 	void selectChannel(int channel) {
+
+#ifdef TRY_REV2_RECOGNITION
+		// check if OTP data is present AND the board is mounted on a r2 carrier
+		auto info = (PortentaBoardInfo*)boardInfo();
+		if (info->magic == 0xB5 && info->carrier == PMC_R2_SKU) {
+			// reverse channels 0 and 2
+			switch (channel) {
+				case 0:
+					channel = 2;
+					break;
+				case 2:
+					channel = 0;
+					break;
+				default:
+					break;
+			}
+		}
+#endif
+#undef TRY_REV2_RECOGNITION
+
 		for (int i=0; i<3; i++) {
 			ch_sel[i] = (i == channel ? 1 : 0);
 		}
