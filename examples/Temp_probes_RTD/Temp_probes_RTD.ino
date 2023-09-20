c0bafb89d0cbd40579a5729860fda6a88a70e659
Leonardo Cavagnis <leonardo.cavagnis@hotmail.it>
2023-09-20 16:01:52 +0200

    removing PT1000 example, not compatible

--- examples/Temp_probes_RTD/Temp_probes_RTD.ino
+++ examples/Temp_probes_RTD/Temp_probes_RTD.ino
@@ -17,10 +17,10 @@
 
 #include <Arduino_MachineControl.h>
 
-// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
+// The value of the Rref resistor. Use 430.0 for PT100
 #define RREF      400.0
 // The 'nominal' 0-degrees-C resistance of the sensor
-// 100.0 for PT100, 1000.0 for PT1000
+// 100.0 for PT100
 #define RNOMINAL  100.0
 
 void setup() {
